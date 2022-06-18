91fdbe08e5298a2913398b1aeebba604cc870fa7
Alexander Novitskiy <alexandernovitsky77@gmail.com>
2022-06-18 18:05:51 +0300

    Update User Claims, Add User to CurrentUserService

--- WhereBy.WebApi/Services/CurrentUserService.cs
+++ WhereBy.WebApi/Services/CurrentUserService.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using WhereBy.Application.Interfaces;
+using WhereBy.Domain;
 
 namespace WhereBy.WebApi.Services
 {
@@ -14,11 +16,14 @@ namespace WhereBy.WebApi.Services
     {
         private readonly IHttpContextAccessor httpContextAccessor;
         private readonly IConfiguration configuration;
+        private readonly IDatabaseContext databaseContext;
 
-        public CurrentUserService(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor,
+            IConfiguration configuration, IDatabaseContext databaseContext)
         {
             this.httpContextAccessor = httpContextAccessor;
             this.configuration = configuration;
+            this.databaseContext = databaseContext;
         }
 
         public int UserId
@@ -26,6 +31,14 @@ namespace WhereBy.WebApi.Services
             get
             {
                 return GetUserIdFromHttpContext();
+            }
+        }
+
+        public User User
+        {
+            get
+            {
+                return databaseContext.Users.FirstOrDefault(u => u.Id == UserId);
             }
         }
 
