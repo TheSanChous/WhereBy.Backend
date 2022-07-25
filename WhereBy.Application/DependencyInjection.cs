2b93dfb30897ac28eb5495f48672d6a6bc11b4de
Alexander Novitskiy <alexandernovitsky77@gmail.com>
2022-07-25 22:03:04 +0300

    WB-5 Create a basic service for sending email

--- WhereBy.Application/DependencyInjection.cs
+++ WhereBy.Application/DependencyInjection.cs
@@ -3,6 +3,10 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using FluentValidation;
 using WhereBy.Application.Common.Behaviors;
+using WhereBy.Application.Interfaces;
+using WhereBy.Application.Common.Services.Mail;
+using WhereBy.Application.Configuration;
+using System.Net;
 
 namespace WhereBy.Application
 {
@@ -11,6 +15,8 @@ namespace WhereBy.Application
         public static IServiceCollection AddApplication(
             this IServiceCollection services)
         {
+            services.AddConfigurations();
+
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services
                 .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
@@ -18,6 +24,9 @@ namespace WhereBy.Application
                 typeof(ValidationBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>),
                 typeof(LoggingBehavior<,>));
+
+            services.AddScoped<IMailService, MailService>();
+
             return services;
         }
     }
