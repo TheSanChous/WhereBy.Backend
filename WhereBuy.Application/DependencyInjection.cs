4cd021f0ce97482efd1f4ef1fbebef5bb23df15e
Alexander Novitskiy <alexandernovitsky77@gmail.com>
2022-07-29 21:54:09 +0300

    WB-22 Move auth code from startup to auth project

--- WhereBuy.Application/DependencyInjection.cs
+++ WhereBuy.Application/DependencyInjection.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using WhereBuy.Application.Common.Behaviors;
+using WhereBuy.Application.Common.Mappings;
 using WhereBuy.Application.Common.Services.Mail;
 using WhereBuy.Common.Abstractions;
 using WhereBuy.Common.Configuration;
@@ -16,13 +17,18 @@ namespace WhereBuy.Application
         {
             services.AddConfigurations();
 
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                config.AddProfile(new AssemblyMappingProfile(typeof(AssemblyMappingProfile).Assembly));
+            });
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services
                 .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
             services.AddTransient(typeof(IPipelineBehavior<,>),
                 typeof(ValidationBehavior<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>),
-            //    typeof(LoggingBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(LoggingBehavior<,>));
 
             services.AddScoped<IMailService, MailService>();
 
